7cdb7054ea795ceb24faa4ce0cd0f25defef28c7
Emmanuel <ajalae2@gmail.com>
2026-02-04 11:08:00 -0100

    flask app

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def get_demand_data():
 
 if __name__ == '__main__':
     # Default Flask port is 5000
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
